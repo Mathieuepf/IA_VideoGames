e00827218f62c1c16d58fd403172235cdc26465c
Mathieuepf <mathieu.roubaud@epfedu.fr>
2024-10-30 16:33:07 +0100

    ajout de random dans le choix de target drone pour les reds drones (evite le stack de drones reds)

--- Assets/Scripts/MyBehaviorTrees/SelectEnemyTurretNearest.cs
+++ Assets/Scripts/MyBehaviorTrees/SelectEnemyTurretNearest.cs
@@ -3,14 +3,15 @@ using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 
 [TaskCategory("MyTasks")]
-[TaskDescription("Select non targeted enemy Drone")]
+[TaskDescription("Select non targeted enemy turret")]
 
-public class SelectEnemyDrone : Action
+public class SelectEnemyTurretNearest : Action
 {
 	IArmyElement m_ArmyElement;
 	public SharedTransform target;
 	public SharedFloat minRadius;
 	public SharedFloat maxRadius;
+	public SharedInt testSharedInt;
 
 	public override void OnAwake()
 	{
@@ -20,10 +21,11 @@ public class SelectEnemyDrone : Action
 	public override TaskStatus OnUpdate()
 	{
 		if (m_ArmyElement.ArmyManager == null) return TaskStatus.Running; // la r�f�rence � l'arm�e n'a pas encore �t� inject�e
-
-		//target.Value = m_ArmyElement.ArmyManager.GetNearestEnemy<Drone>(transform.position,minRadius.Value,maxRadius.Value)?.transform;
 		
-		target.Value = m_ArmyElement.ArmyManager.GetRandomEnemy<Drone>(transform.position,minRadius.Value,maxRadius.Value)?.transform;
+		if (target.Value != null) return TaskStatus.Success; // Si target toujours en vie, garder la meme target
+		
+		//target.Value = m_ArmyElement.ArmyManager.GetRandomEnemy<Turret>(transform.position,minRadius.Value,maxRadius.Value)?.transform;
+		target.Value = m_ArmyElement.ArmyManager.GetNearestEnemy<Turret>(transform.position,minRadius.Value,maxRadius.Value)?.transform;
 		
 		if (target.Value != null) return TaskStatus.Success;
 		else return TaskStatus.Failure;
